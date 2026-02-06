e7c6881d2f65afe61cc2e3438fd8ef1c430034b1
Vnxcarvalho <alanviniciuscarvalho@gmail.com>
2026-02-06 00:54:08 -0300

    commit03

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ export function Header() {
             <img
               src={logo}
               alt="Delicatta Joalheria"
-              className="h-20 w-auto rounded-full object-cover md:h-20"
+              className="h-16 w-auto rounded-full object-cover md:h-18"
             />
           </a>
 
